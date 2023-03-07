855fa46a401e67fa9dbe32001fbd04bd6d47ac31
Scifiler-sys <pagdilaostephen@gmail.com>
2023-03-07 17:24:25 -0500

    cleaning some stuff

--- src/test/java/com/revature/pokemondemo/TrainerControllerTest.java
+++ src/test/java/com/revature/pokemondemo/TrainerControllerTest.java
@@ -13,17 +13,13 @@ import static org.mockito.ArgumentMatchers.anyInt;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootTest
 @AutoConfigureMockMvc
