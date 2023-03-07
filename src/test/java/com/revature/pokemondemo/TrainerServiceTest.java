26a19a9d2370bdce63b35a15f008a83279835e9d
Scifiler-sys <pagdilaostephen@gmail.com>
2023-03-07 15:45:24 -0500

    added more unit tests

---
+++ src/test/java/com/revature/pokemondemo/TrainerServiceTest.java
@@ -0,0 +1,113 @@
+package com.revature.pokemondemo;
+
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Optional;
+
+import com.revature.pokemondemo.model.Trainer;
+import com.revature.pokemondemo.repository.PokemonRepository;
+import com.revature.pokemondemo.repository.TrainerRepository;
+import com.revature.pokemondemo.service.TrainerService;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+
+@SpringBootTest
+public class TrainerServiceTest {
+    
+    @Autowired
+    private TrainerService trainerService;
+
+    //MockBean mocks the class
+    @MockBean
+    private TrainerRepository trainRepo;
+
+    @MockBean
+    private PokemonRepository pokeRepo;
+
+    private Trainer trainerDummy;
+
+    //Runs before each unit test
+    //Cleans our trainerDummy object in the event it was touched by another unit test
+    @BeforeEach
+    public void initialSetup()
+    {
+        String name = "Stephen", username = "stephen", password = "stephen123";
+        Trainer trainerDummy = new Trainer();
+        trainerDummy.setName(name);
+        trainerDummy.setUsername(username);
+        trainerDummy.setPassword(password);
+    }
+
+    @Test
+    public void Find_Trainer_Should_Work_With_Existing_Trainer()
+    {
+        //Arrange
+        Mockito.when(trainRepo.findByUsernameAndPassword(trainerDummy.getUsername(), trainerDummy.getPassword()))
+            .thenReturn(Optional.ofNullable(trainerDummy));
+
+        //Act
+        Trainer actualTrainer = trainerService.findTrainer(trainerDummy);
+
+        //Assert
+        Assertions.assertNotNull(actualTrainer);
+        Assertions.assertEquals(trainerDummy.getName(), actualTrainer.getName());
+    }
+
+    @Test
+    public void Find_Trainer_Should_Fail_With_No_Trainer_Found()
+    {
+        //Arrange
+        Mockito.when(trainRepo.findByUsernameAndPassword(trainerDummy.getUsername(), trainerDummy.getPassword()))
+            .thenReturn(Optional.empty());
+        
+        //Act
+        Trainer actualTrainer = trainerService.findTrainer(trainerDummy);
+
+        //Assert
+        Assertions.assertNotNull(actualTrainer);
+        Assertions.assertEquals(actualTrainer.getName(), "Trainer was not found");
+    }
+
+    @Test
+    public void Add_Resource_Should_Work_With_Not_Null_Resource()
+    {
+        //Arrange
+        Mockito.when(trainRepo.save(trainerDummy))
+            .thenReturn(trainerDummy);
+
+        //Act
+        Trainer actualTrainer = trainerService.addResource(trainerDummy);
+
+        //Assert
+        Assertions.assertNotNull(actualTrainer);
+        Assertions.assertEquals(actualTrainer.getName(), trainerDummy.getName());
+    }
+
+    @Test
+    public void Add_Resource_Should_Fail_With_Null_Resource()
+    {
+        //Arrange
+        Mockito.when(trainRepo.save(trainerDummy))
+            .thenReturn(trainerDummy);
+        
+        //Act
+        Trainer actualTrainer = trainerService.addResource(trainerDummy);
+
+        //Assert
+        Assertions.assertNotNull(actualTrainer);
+        Assertions.assertEquals(actualTrainer.getName(), "Trainer added was null");
+    }
+
+    @Test
+    public void Add_Pokemon_Should_Work_With_Existing_User()
+    {
+        
+    }
+}
